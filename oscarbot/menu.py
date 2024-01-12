5a90be6fe28ace9eba96c914c8b57972cb27837a
Oleg Maslov <maslovoa.ge@gmail.com>
2024-01-12 12:23:41 +0300

    add docker and test files

--- oscarbot/menu.py
+++ oscarbot/menu.py
@@ -24,9 +24,16 @@ class Button:
 
 class Menu:
 
-    def __init__(self, button_list: list, buttons_in_line=1):
+    def __init__(self, button_list: list, buttons_in_line=1, mode='inline'):
+        """
+
+        @param button_list:
+        @param buttons_in_line:
+        @param mode: inline or keyboard
+        """
         self.button_list = button_list
         self.buttons_in_line = buttons_in_line
+        self.mode = mode
 
     def build(self):
         menu_items = []
@@ -40,5 +47,11 @@ class Menu:
                 i = 0
                 line_menu_items = []
         menu_items.append(line_menu_items)
-        # return json.dumps({'inline_keyboard': menu_items})
-        return json.dumps({'inline_keyboard': menu_items})
+        if self.mode == 'inline':
+            return json.dumps({'inline_keyboard': menu_items})
+        else:
+            return json.dumps({
+                'keyboard': menu_items,
+                'resize_keyboard': True,
+                'one_time_keyboard': True
+            })
