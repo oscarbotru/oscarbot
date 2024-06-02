75bbd89c6758cf831de8de3c54763bb281ec1c1d
revike <revike@ya.ru>
2024-06-03 01:18:19 +0300

    refactoring runbot

--- oscarbot/services.py
+++ oscarbot/services.py
@@ -4,8 +4,9 @@ from django.conf import settings
 
 def get_bot_model():
     try:
-        app_name, app_model = settings.OSCARBOT_BOT_MODEL.split('.')
+        *app_name_lst, app_model = settings.OSCARBOT_BOT_MODEL.split('.')
+        app_name = '.'.join(app_name_lst)
         bot_model = apps.get_model(app_name, app_model)
         return bot_model
     except Exception:
-        raise RuntimeError('Failed to get Bot model')
+        raise RuntimeError('Failed to get Bot model. Add to settings.py OSCARBOT_BOT_MODEL = app.BotModel')
